12d346cd67907860dfa4aed69d7d6ea74d564c44
Pawel Kwiecinski <pawel_kwiecinski@epam.com>
2023-10-10 14:33:24 +0200

    Initial commit

---
+++ src/test/java/com/example/rpc/RpcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class RpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
